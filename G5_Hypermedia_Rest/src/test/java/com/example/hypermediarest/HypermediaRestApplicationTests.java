47a59bbcd3d9814db2fa488deb708cc28c9e84f2
User from script <asedkov@gmail.com>
2021-05-11 13:40:34 +0300

    +5 Hypermedia Rest
    Building a Hypermedia-Driven RESTful Web Service

---
+++ G5_Hypermedia_Rest/src/test/java/com/example/hypermediarest/HypermediaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hypermediarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermediaRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
