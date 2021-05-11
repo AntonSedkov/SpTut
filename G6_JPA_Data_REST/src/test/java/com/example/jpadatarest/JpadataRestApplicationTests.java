2d5f2f676c2b48ea63a42b0be05e7fa34536143b
User from script <asedkov@gmail.com>
2021-05-11 14:31:09 +0300

    +6 JPA Data REST
    Accessing JPA Data with REST

---
+++ G6_JPA_Data_REST/src/test/java/com/example/jpadatarest/JpadataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpadatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpadataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
