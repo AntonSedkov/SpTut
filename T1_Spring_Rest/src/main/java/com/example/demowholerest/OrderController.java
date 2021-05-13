d77cd4cc7bcb6741cc9ecb3073a9d429a77c43b3
User from script <S3dk0vAG@gmail.com>
2021-05-14 00:33:19 +0300

    + Complete tutorial
    Throughout this tutorial, you have engaged in various tactics to build REST APIs. As it turns out, REST isn’t just about pretty URIs and returning JSON instead of XML.
    
    Instead, the following tactics help make your services less likely to break existing clients you may or may not control:
    
    Don’t remove old fields. Instead, support them.
    
    Use rel-based links so clients don’t have to hard code URIs.
    
    Retain old links as long as possible. Even if you have to change the URI, keep the rels so older clients have a path onto the newer features.
    
    Use links, not payload data, to instruct clients when various state-driving operations are available.
    
    It may appear to be a bit of effort to build up RepresentationModelAssembler implementations for each resource type and to use these components in all of your controllers. But this extra bit of server-side setup (made easy thanks to Spring HATEOAS) can ensure the clients you control (and more importantly, those you don’t) can upgrade with ease as you evolve your API.

--- T1_Spring_Rest/src/main/java/com/example/demowholerest/OrderController.java
+++ T1_Spring_Rest/src/main/java/com/example/demowholerest/OrderController.java
@@ -2,6 +2,10 @@ package com.example.demowholerest;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,6 +53,46 @@ public class OrderController {
         return ResponseEntity
                 .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
                 .body(assembler.toModel(newOrder));
+    }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(
+            @PathVariable
+                    Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed") //
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
+    }
+
+    @DeleteMapping("/orders/{id}/cancel")
+    ResponseEntity<?> cancel(
+            @PathVariable
+                    Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.CANCELLED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed")
+                        .withDetail("You can't cancel an order that is in the "
+                                + order.getStatus() + " status"));
     }
 
 }
