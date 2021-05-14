bbe5a9a613d501646ae7a0ce0dab333688967745
User from script <S3dk0vAG@gmail.com>
2021-05-14 21:16:21 +0300

    Tutorial 2: Boot + Security +Angular
    Tutorial 2: Boot + Security +Angular

---
+++ T2_Security_Angular/src/main/java/com/example/securityangular/SecurityangularApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityangularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityangularApplication.class, args);
+    }
+
+}
---
+++ T2_Security_Angular/src/test/java/com/example/securityangular/SecurityangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityangularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
